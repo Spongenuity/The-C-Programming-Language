28576d195ca7c535add6b02f86790d3c7a799956
Samer Dabra <samer.dabra@googlemail.com>
2017-12-11 19:26:51 +0000

    Intro Complete

--- lessons/c-intro/add/add.c
+++ lessons/c-intro/add/add.c
@@ -14,17 +14,27 @@
  *   ./add
  */
 #include <stdio.h>
+#include <math.h>
 
 int add(int a, int b)
 {
     return a + b;
 }
 
+int intsqrt (int x)
+{
+    return sqrt(x);
+}
+
 int main(void)
 {
     int result = add(3, 4);
 
-    printf("3 + 4 = %d", result);
+    printf("3 + 4 = %d\n", result);
+
+
+    double squrtres = intsqrt(81);
+    printf("Square root of 81 is %lf\n", squrtres );
 
     return 0;
 }
--- lessons/learning-new-languages/hello/hello.c
+++ lessons/learning-new-languages/hello/hello.c
@@ -14,11 +14,21 @@
  *   ./hello
  */
 
+
+
  #include <stdio.h>
+ #include <math.h>
+
+ int sqroot()
+{
+    printf("Square root of %lf is %lf\n", 128934.0, sqrt(128934.0) );
+    return 0;
+}
 
  int main(void)
  {
      printf("Hello, world!\n");
+     sqroot();
 
      return 0;
  }
